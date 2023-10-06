ea80252619fd4631bc76ed3f5a0aba11df91de07
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-10-06 11:22:01 +0700

    add toast

--- Reader/src/slices/requestApi.js
+++ Reader/src/slices/requestApi.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { setCurrentAction, loginBegin, loginFailure, loginSuccess, registerBegin, registerFailure, registerSuccess } from './authSlice'
 
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 export const loginUser = async (user, dispatch, navigate) => {
     dispatch(loginBegin());
     try {
         const res = await axios.post('http://localhost:8000/api/auth/login', user);
         dispatch(loginSuccess(res.data));
+        toast.success('Login successfully!');
         navigate('/');
     } catch (err) {
         console.log(err.response.data);
         dispatch(loginFailure());
+        toast.error('Login failed!');
     }
 }
 
@@ -19,7 +24,9 @@ export const registerUser = async (user, dispatch) => {
         await axios.post('http://localhost:8000/api/auth/register', user);
         dispatch(registerSuccess());
         dispatch(setCurrentAction('Sign In'))
+        toast.success('Sign up successfully!');
     } catch (err) {
         dispatch(registerFailure());
+        toast.error(err.response.data);
     }
 }
