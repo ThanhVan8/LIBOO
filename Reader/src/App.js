4a636057cc7283732f2e9ad5f08a387654afd1f4
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-10-06 11:21:11 +0700

    config private route

--- Reader/src/App.js
+++ Reader/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom'
 import Auth from './pages/Auth'
 import Home from './pages/Home';
+import PrivateRoute from './routers/PrivateRoute';
 
 const App = () => {
   return (
     <div className="w-screen h-screen flex">
       <Routes>
         <Route path="/Auth" element={<Auth />} />
-        <Route path="/" element={<Home />} />
+        <Route element={<PrivateRoute />} >
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </div>
   );
