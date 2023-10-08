4b407532cb98206748fcdd11edee363c401168a2
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-10-08 17:36:08 +0700

    limit date range in borrow form

--- Reader/src/pages/Borrow.jsx
+++ Reader/src/pages/Borrow.jsx
@@ -2,20 +2,25 @@ import React, {useState} from 'react'
 import {Input} from "@material-tailwind/react";
 import {useParams} from 'react-router-dom'
 import CustomButton from '../components/CustomButton';
+import { useEffect } from 'react';
+
+const EXPIRATION = 7;
 
 const Borrow = () => {
   const {id} = useParams()
 
-  const [slip, setSlip] = useState({_id:'', username:'vann26', isbn: id, borrowDate: '', dueDate: ''})
+  const [slip, setSlip] = useState({_id:'', username:'vann26', isbn: id ? id : '', borrowDate: '', dueDate: ''})
   
   const handleChangeInfo = (e) => {
     e.preventDefault();
     const {name, value} = e.target;
-    setSlip({...slip, [name]: value});
+    const chosenDate = new Date(value);
+    setSlip({...slip, borrowDate: value, dueDate: new Date(chosenDate.getFullYear(), chosenDate.getMonth(), chosenDate.getDate() + EXPIRATION).toISOString().split('T')[0]});
   }
 
   const handleBorrow = (e) => {
     e.preventDefault();
+    console.log(slip)
   }
 
   return (
@@ -41,8 +46,10 @@ const Borrow = () => {
             label="Received date (in 3 days)"
             name='borrowDate'
             value={slip.borrowDate}
-            type='date'
             onChange={handleChangeInfo}
+            type='date'
+            min={(new Date()).toISOString().split('T')[0]}
+            max={new Date(new Date().getTime() + (2 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0]}
             required
           />
           <Input
@@ -54,7 +61,7 @@ const Borrow = () => {
           />
         </div>
         <div className='flex justify-center'>
-          <CustomButton label='done' type='submit' />
+          <CustomButton label='done' type='submit' disabled={id ? false : true} />
         </div>
       </form>
 
