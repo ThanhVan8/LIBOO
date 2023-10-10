39b10ae3cda3cf2bae9218c3963628a2d941eda4
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-10-10 17:34:38 +0700

    fix font-size homepage

--- Reader/src/pages/Home.jsx
+++ Reader/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
 
       <div className='self-center w-1/2 space-y-8 pl-16'>
         <p className='text-5xl text-red font-semibold'>LIBOO</p>
-        <p className='text-justify font-medium'>LIBOO is not only a repository of information but also a vibrant community hub where individuals of all ages can come together to read, study, research, and connect. Join us in this virtual realm of literary exploration and convenience, where your love for books meets the convenience of the digital age.</p>
+        <p className='text-justify text-base'>LIBOO is not only a repository of information but also a vibrant community hub where individuals of all ages can come together to read, study, research, and connect. Join us in this virtual realm of literary exploration and convenience, where your love for books meets the convenience of the digital age.</p>
         <CustomButton classes='w-32' label='Explore' icon={<BiChevronRight color='white' size='1.5rem' />} onClick={navigateToCatalog} />
       </div>
     </div>
