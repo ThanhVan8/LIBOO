837ca668c5666bd108009990c981cba4f9400425
Thanh Van <tttvan21@clc.fitus.edu.vn>
2023-10-10 17:11:25 +0700

    fe-homepage

--- Reader/src/components/CustomButton.jsx
+++ Reader/src/components/CustomButton.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Button } from '@material-tailwind/react'
 
-const CustomButton = ({label, onClick, type, classes, disabled}) => {
+const CustomButton = ({label, onClick, type, classes, disabled, icon}) => {
   return (
     <Button 
-      className={`w-[18.75rem] h-10 ${classes}`} 
+      className={`flex items-center justify-center w-[18.75rem] h-10 ${classes}`} 
       style={{backgroundImage: `linear-gradient(to right, var(--my-red), var(--my-orange)`}}
       onClick={onClick}
       type={type}
       disabled={disabled}
     >
       {label}
+      {icon}
     </Button>
   )
 }
